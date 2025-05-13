fbe70e2dad97ba3e3d50493cf40425e6eb6d7be7
shredder42 <atbinder42@gmail.com>
2025-05-13 17:18:58 -0500

    server

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+
+	serverMux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: serverMux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+
+}
