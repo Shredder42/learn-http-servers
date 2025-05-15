01240970f00c74dccdee8b6084a725830f00c3a8
shredder42 <atbinder42@gmail.com>
2025-05-15 12:48:51 -0500

    custom handlers

--- main.go
+++ main.go
@@ -7,19 +7,27 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	serverMux := http.NewServeMux()
+
+	serverMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	serverMux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK\n"))
+	})
 
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: serverMux,
 	}
+
+	log.Printf("Serving on port: %s\n", port)
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	log.Printf("Serving on port: %s\n", port)
 
 }
