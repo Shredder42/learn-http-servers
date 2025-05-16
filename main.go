19f11ce5a335aa39f91262b7fd544062b5ab2589
shredder42 <atbinder42@gmail.com>
2025-05-16 16:38:42 -0500

    admin namespace

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ func main() {
 	serverMux := http.NewServeMux()
 	serverMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	serverMux.HandleFunc("GET /api/healthz", handlerReadiness)
-	serverMux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
-	serverMux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
+	serverMux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	serverMux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -36,19 +36,4 @@ func main() {
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Cache-Control", "no-cache")
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, req)
-
-	})
-}
-
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
 }
