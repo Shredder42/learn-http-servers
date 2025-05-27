8ad09965775fcea66b483e66d4508e5a09aa0692
shredder42 <atbinder42@gmail.com>
2025-05-27 15:11:48 -0500

    JWTs

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,11 @@
 package auth
 
 import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -15,4 +20,56 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(hash, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	timeNow := jwt.NewNumericDate(time.Now().UTC())
+	timeExpires := jwt.NewNumericDate(time.Now().UTC().Add(expiresIn))
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  timeNow,
+		ExpiresAt: timeExpires,
+		Subject:   userID.String(),
+	})
+
+	tokenString, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, &claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	if !token.Valid {
+		return uuid.UUID{}, errors.New("invalid token")
+	}
+
+	// issuer, err := claims.GetIssuer() -- this also works
+	issuer, err := token.Claims.GetIssuer()
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	if issuer != "chirpy" {
+		return uuid.UUID{}, errors.New("invalid issuer")
+	}
+
+	// userID, err := claims.GetSubject() -- this also works
+	userID, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	return uuid.Parse(userID)
+
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,28 +2,69 @@ package auth
 
 import (
 	"testing"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 func TestHashPasswordMatch(t *testing.T) {
-	hash, err := HashPassword("baseball42")
-	if err != nil {
-		t.Error("couldn't hash password", err)
-	}
+	wantErr := false
+	hash, _ := HashPassword("baseball42")
 
-	err = CheckPasswordHash(hash, "baseball42")
-	if err != nil {
+	err := CheckPasswordHash(hash, "baseball42")
+	if (err != nil) != wantErr {
 		t.Error("password does not match hash", err)
 	}
 }
 
 func TestHashPasswordWrong(t *testing.T) {
-	hash, err := HashPassword("doggyboy")
-	if err != nil {
-		t.Error("couldn't hash password", err)
-	}
+	wantErr := true
+	hash, _ := HashPassword("doggyboy")
 
-	err = CheckPasswordHash(hash, "doggyboyz")
-	if err != nil {
+	err := CheckPasswordHash(hash, "doggyboyz")
+	if (err != nil) != wantErr {
 		t.Error("password does not match hash", err)
+	}
+}
+
+func TestJWTCorrect(t *testing.T) {
+	wantErr := false
+	userID := uuid.New()
+	tokenSecret := "ghost-is-the-best-jwt-key-ever-120723"
+
+	tokenString, _ := MakeJWT(userID, tokenSecret, 1*time.Second)
+
+	_, err := ValidateJWT(tokenString, tokenSecret)
+	if (err != nil) != wantErr {
+		t.Error("secret does not match", err)
+	}
+}
+
+func TestJWTTimeOut(t *testing.T) {
+	wantErr := true
+	userID := uuid.New()
+	tokenSecret := "ghost-is-the-best-jwt-key-ever-120723"
+
+	tokenString, _ := MakeJWT(userID, tokenSecret, 1*time.Second)
+
+	time.Sleep(2 * time.Second)
+
+	_, err := ValidateJWT(tokenString, tokenSecret)
+	if (err != nil) != wantErr {
+		t.Error("JWT expired", err)
+	}
+}
+
+func TestJWTSecretWrong(t *testing.T) {
+	wantErr := true
+	userID := uuid.New()
+	tokenSecret := "ghost-is-the-best-jwt-key-ever-120723"
+	otherSecret := "shredder-is-the-best-jwt-key-ever-120723"
+
+	tokenString, _ := MakeJWT(userID, tokenSecret, 1*time.Second)
+
+	_, err := ValidateJWT(tokenString, otherSecret)
+	if (err != nil) != wantErr {
+		t.Error("secret does not match", err)
 	}
 }
