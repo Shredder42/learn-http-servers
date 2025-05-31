b651a0b3a1e1c18980ef132e0af914ad8a3ca5c4
shredder42 <atbinder42@gmail.com>
2025-05-31 17:53:38 -0500

    documentation

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -3,18 +3,37 @@ package main
 import (
 	"net/http"
 
+	"github.com/Shredder42/learn-http-servers/internal/database"
 	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerRetrieveChirps(w http.ResponseWriter, req *http.Request) {
-	dbChirps, err := cfg.db.GetChirps(req.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Error retrieving chirps", err)
+	authorID := req.URL.Query().Get("author_id")
+	dbChirps := []database.Chirp{} // not sure about this underline. might be better to use var dbChirps []database.Chirp
+	Chirps := []Chirp{}
+	var err error
+
+	if len(authorID) > 0 {
+		parsedUserID, parseErr := uuid.Parse(authorID)
+		if parseErr != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid user ID", parseErr)
+			return
+		}
+		dbChirps, err = cfg.db.GetChirpsByUser(req.Context(), parsedUserID)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Error retrieving chirps", err)
+			return
+		}
+	} else {
+		dbChirps, err = cfg.db.GetChirps(req.Context())
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Error retrieving chirps", err)
+			return
+		}
 	}
 
-	newChirps := []Chirp{}
 	for _, dbChirp := range dbChirps {
-		newChirps = append(newChirps, Chirp{
+		Chirps = append(Chirps, Chirp{
 			ID:        dbChirp.ID,
 			CreatedAt: dbChirp.CreatedAt,
 			UpdatedAt: dbChirp.UpdatedAt,
@@ -23,7 +42,7 @@ func (cfg *apiConfig) handlerRetrieveChirps(w http.ResponseWriter, req *http.Req
 		})
 	}
 
-	respondWithJSON(w, http.StatusOK, newChirps)
+	respondWithJSON(w, http.StatusOK, Chirps)
 
 }
 
